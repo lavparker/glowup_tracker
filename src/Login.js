10a8cb7b1bf20da5b80223c9c4d38dc9a9652326
La Vinia Parker <lavinia.parker@outlook.com>
2024-01-16 17:50:44 -0500

    login form frontend form

--- src/Login.js
+++ src/Login.js
@@ -17,13 +17,69 @@ const Login = () => {
         setErrMsg('');
     }, [user, pwd])
 
-    return (
-        <section>
-            //error message display 
+    const handleSubmit = async (e) =>{
+        e.preventDefault(); 
+        setSuccess(true); 
+        console.log(user, pwd);
+        setUser('');
+        setPwd('');
+        setSuccess(true);
+    }
 
+    return (
+        <>
+
+        {success ? (
+            <section>
+                <h1>You are logged in!</h1>
+                <br />
+                <p>
+                    <a href="#"> Go to Home</a>
+                </p>
+            </section>
+        ) : (
+
+      
+        <section>
+            //error message display //assertive will have the error display immediately
             <p ref={errRef} className={errMsg ? "effmsg" : "offscreen"} arialive="assertive">{errMsg}</p>
 
+            <h1>Sign In</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="username">UserName:</label>
+                <input 
+                    type="text" 
+                    id="username" 
+                    ref={userRef}
+                    autoComplete='off'
+                    onChange={(e) => setUser(e.target.value)}
+                    value={user} //makes this a controlled input allows us to clear input upon submission 
+                    required 
+                    />
+                <label htmlFor="password">Password:</label>
+                <input 
+                    type="password" 
+                    id="password" 
+                    ref={userRef}
+                    autoComplete='off'
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={pwd} //makes this a controlled input allows us to clear input upon submission 
+                    required 
+                    />
+
+                    <button>Sign In</button>
+            </form>
+            <p>
+                Need an Account? <br />
+                <span className='line'>
+                    {/*put router link here */}
+                    <a href="#"> Sign Up </a>
+                </span>
+            </p>
+
         </section>
+          )}
+          </>
     )
 }
 
