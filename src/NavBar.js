1e0e5153e210722ad7815e87d44e8e8d1c70f0cc
La Vinia Parker <lavinia.parker@outlook.com>
2024-01-23 15:19:43 -0500

    added images for home screen

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,23 +1,41 @@
-//Home - link that doesn't work yet 
-import React from 'react'; 
-import { ReactDOM } from 'react';
-import './NavBar.css'; 
+import React from 'react';
+import './NavBar.css';
 
 function NavBar(props) {
 
-    const glowups = ["workout", "measurements", "weather", "music", "todo", "schedule", "notes", "quotePhoto", "mealPlan", "speak", "habitMood" ]; 
+    const glowups = [
+        {id: 1, glowup: "workout", image: "workout.png"},
+        {id: 2, glowup: "measurements", image: "measurements.png"},
+        {id: 3, glowup: "weather", image: "weather.png"},
+        {id: 4, glowup: "music", image: "music.png"},
+        {id: 5, glowup: "todo", image: "todolist.png"},
+        {id: 6, glowup: "schedule", image: "schedule.png"},
+        {id: 7, glowup: "notes", image: "notes.png"},
+        {id: 8, glowup: "quotePhoto", image: "quote.png"},
+        {id: 9, glowup: "mealPlan", image: "mealPlan.png"},
+        {id: 10, glowup: "habitMood", image: "mood.png"},
+        {id: 11, glowup: "selfCare", image: "selfcare.png"}
+    ];
 
-
-    return(
+    return (
         <>
             <h1>Select Your Goal</h1>
 
             <ul>
-                {glowups.map((glowup) => <div className='goal-square'>{glowup}</div>)}
+                {glowups.map((goal) => (
+                    <div className='goal-square' key={goal.id}>
+                        {goal.glowup}
+                        <img src={require(`./images/${goal.image}`).default} alt={`${goal.glowup} Image`} />
+                        hello
+                    </div>
+                ))}
             </ul>
+
+            
         </>
+
+
     )
 }
 
-
-export default NavBar; 
+export default NavBar;
---
+++ src/Workout.jsx
@@ -0,0 +1,13 @@
+function Workout () {
+    return(
+        <form action="">
+            <h1>Hi, User! What is your workout vibe today</h1>
+
+            <label htmlFor="">I don't want to do this but I'm showing up today</label>
+            <label htmlFor="">I'm feelin' it, but I don't want to go too hard</label>
+            <label htmlFor="">Challenge me!</label>
+  
+        </form>
+    )
+
+}
